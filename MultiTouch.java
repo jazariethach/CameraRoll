ce4f5e152663d0d5e83d3e2440a6efaddaa39214
jazariethach <jazariethach01@gmail.com>
2015-05-21 17:37:54 -0700

    Need to fix focal point, launching and touch almost working

--- MultiTouch.java
+++ MultiTouch.java
@@ -8,12 +8,13 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.FloatMath;
 import android.view.Display;
-import android.view.GestureDetector;
 import android.net.Uri;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,63 +25,39 @@ import android.widget.ImageView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MultiTouch extends ActionBarActivity {
-    //MultiTouch imageview;
     public static TouchView imageview;
     public static Drawable d;
-    Bitmap bmap;
     public static Matrix matrix;
     public static Matrix savedMatrix;
-    private static final int NONE = 0;
-    private static final int DRAG = 1;
-    private static final int ZOOM = 2;
-    private int mode = NONE;
-    private PointF start = new PointF();
-    private PointF mid = new PointF();
-    private float oldDist = 1f;
-    private float d_idk = 0f;
-    private float newRot = 0f;
-    private float[] lastEvent = null;
-    public static float mFocusX = 0.f;
-    public static float mFocusY = 0.f;
 
-    // float scale = 1f;
-    ScaleGestureDetector SGD;
-
-    @Override/**/
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
+//        imageview = new TouchView(this);
+//        imageview.setScaleType(ImageView.ScaleType.MATRIX);
         imageview = (TouchView) findViewById(R.id.launchedPic);
-//        imageview = (MultiTouch) findViewById(R.id.launchedPic);
-       // SGD = new ScaleGestureDetector(this,new ScaleListener());
-        //bitmap factory decode function
-        //setimagebitmap
 
         Bundle extra = getIntent().getExtras();
         if (extra != null) {
             Uri uri = Uri.parse(extra.getString("imageUri"));
 
-            imageview.setImageURI(uri);
-          /*  File myFile = new File(uri.getPath());
+            File myFile = new File(uri.getPath());
             d = Drawable.createFromPath(myFile.getAbsolutePath());
-           */
+
             matrix = new Matrix();
             savedMatrix = new Matrix();
-          /*  Display display = getWindowManager().getDefaultDisplay();
-            mFocusX = display.getWidth()/2f;
-            mFocusY = display.getHeight()/2f;
-            */
-            //imageview.setImageDrawable(getResources().getDrawable(R.drawable.launchedPic));
-          //  imageview.buildDrawingCache();
-           // bmap = Bitmap.createBitmap(imageview.getDrawingCache());
-            //imageview.buildDrawingCache(true);
-            //bmap = Bitmap.createBitmap(imageview.getDrawingCache());
-           // imageview.setImageBitmap(bmap);
+            Display display = getWindowManager().getDefaultDisplay();
+
+            imageview.setImageURI(uri);
+
+            //setContentView(imageview);
         }
 
     }
@@ -99,7 +76,6 @@ public class MultiTouch extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         if (id == R.id.load) {
             load();
             return true;
@@ -112,23 +88,29 @@ public class MultiTouch extends ActionBarActivity {
     }
 
   public static class TouchView extends ImageView {
-      private Drawable mIcon;
       private float x = 0;
       private float y = 0;
-
-
-      Bitmap bImage;
+      Bitmap bmap;
       int height;
       int width;
       Paint paint;
-      GestureDetector gestures;
-      ScaleGestureDetector scaleGesture;
       private int mActivePointerId;
-      private List<Point> circlePoints;
+      private List<Point> dots;
       private ScaleGestureDetector mScaleDetector;
       private float mScaleFactor = .4f;
       private int mImageHeight, mImageWidth;
-
+      private static final int NONE = 0;
+      private static final int DRAG = 1;
+      private static final int ZOOM = 2;
+      private int mode = NONE;
+      private PointF start = new PointF();
+      private PointF mid = new PointF();
+      private float oldDist = 1f;
+      private float d_idk = 0f;
+      private float newRot = 0f;
+      private float[] lastEvent = null;
+      private static final int MAX_CLICK_DURATION = 100;
+      private long startClickTime;
 
       public TouchView(Context context) {
         super(context);
@@ -139,6 +121,12 @@ public class MultiTouch extends ActionBarActivity {
 
         super(context, attrs, defStyle);
         //Bitmap bm=((BitmapDrawable)this.getDrawable()).getBitmap();
+        paint = new Paint();
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.RED);
+        // paint.setARGB(150,255,0,0);
+        paint.setStyle(Paint.Style.STROKE);
+        dots = new ArrayList<Point>();
     }
 
 
@@ -148,25 +136,9 @@ public class MultiTouch extends ActionBarActivity {
           paint = new Paint();
           paint.setStrokeWidth(10);
           paint.setColor(Color.RED);
+         // paint.setARGB(150,255,0,0);
           paint.setStyle(Paint.Style.STROKE);
-          circlePoints = new ArrayList<Point>();
-
-       /*   mImageHeight 	= d.getIntrinsicHeight();
-          mImageWidth 	= d.getIntrinsicWidth();
-          float scaledImageCenterX = (mImageWidth*mScaleFactor)/2;
-          float scaledImageCenterY = (mImageHeight*mScaleFactor)/2;
-
-          matrix.postScale(mScaleFactor, mScaleFactor);
-          matrix.postTranslate(mFocusX - scaledImageCenterX, mFocusY - scaledImageCenterY);
-          imageview.setImageMatrix(matrix);
-          */
-          // this.buildDrawingCache();//How to get bitmap or view????
-          //bImage = this.getDrawingCache();
-
-          //Bitmap bm=((BitmapDrawable)this.getDrawable()).getBitmap();
-          //   Bitmap bitmap = Bitmap.createBitmap(this.getWidth(),this.getHeight(), Bitmap.Config.RGB_565);
-          //  Canvas canvas = new Canvas(bitmap);
-          //this.draw(canvas);
+          dots = new ArrayList<Point>();
       }
 
       @Override
@@ -175,65 +147,116 @@ public class MultiTouch extends ActionBarActivity {
           height = this.getMeasuredHeight();
           width = this.getMeasuredWidth();
           //canvas.setBitmap(bImage);
-          for (Point p : circlePoints) {
+          for (Point p : dots) {
               canvas.drawCircle(p.x, p.y, 2, paint);
           }
       }
-
+//launchmode single instance
       @Override
       public boolean onTouchEvent( MotionEvent event) {
-     /*     mScaleDetector.onTouchEvent(event);
-          mActivePointerId = event.getPointerId(0); // 0 == pointer index
-          float scaledImageCenterX = (mImageWidth*mScaleFactor)/2;
-          float scaledImageCenterY = (mImageHeight*mScaleFactor)/2;
-
-          matrix.reset();
-          matrix.postScale(mScaleFactor, mScaleFactor);*/
           //canvas get matrix
           //postrotate
           //bundle canvas  state, save and restore
           //restore
-
-          if (event.getAction() == MotionEvent.ACTION_DOWN) {
-              x = event.getX();
-              y = event.getY();
-              circlePoints.add(new Point(Math.round(x), Math.round(y)));
-
-              invalidate();
+          switch (event.getAction() & MotionEvent.ACTION_MASK) {
+              case MotionEvent.ACTION_DOWN:
+                  startClickTime = Calendar.getInstance().getTimeInMillis();
+                  savedMatrix.set(matrix);
+                  start.set(event.getX(), event.getY());
+                  mode = DRAG;
+                  lastEvent = null;
+                  break;
+              case MotionEvent.ACTION_POINTER_DOWN:
+                  oldDist = spacing(event);
+                  if (oldDist > 10f) {
+                      savedMatrix.set(matrix);
+                      midPoint(mid, event);
+                      mode = ZOOM;
+                  }
+                  lastEvent = new float[4];
+                  lastEvent[0] = event.getX(0);
+                  lastEvent[1] = event.getX(1);
+                  lastEvent[2] = event.getY(0);
+                  lastEvent[3] = event.getY(1);
+                  d_idk = rotation(event);
+                  break;
+              case MotionEvent.ACTION_UP:
+                  long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
+                  if(clickDuration < MAX_CLICK_DURATION) {
+                      x = event.getX();
+                      y = event.getY();
+                      //if statement to check width and height of image
+                      dots.add(new Point(Math.round(x), Math.round(y)));
+                      invalidate();
+                  }
+                  break;
+              case MotionEvent.ACTION_POINTER_UP:
+                  mode = NONE;
+                  lastEvent = null;
+                  break;
+              case MotionEvent.ACTION_MOVE:
+                  if (mode == DRAG) {
+                      matrix.set(savedMatrix);
+                      float dx = event.getX() - start.x;
+                      float dy = event.getY() - start.y;
+                      matrix.postTranslate(dx, dy);
+                  } else if (mode == ZOOM) {
+                      float newDist = spacing(event);
+                      if (newDist > 10f) {
+                          matrix.set(savedMatrix);
+                          float scale = (newDist / oldDist);
+                          matrix.postScale(scale, scale, mid.x, mid.y);
+                      }
+                      if (lastEvent != null && event.getPointerCount() == 2) {
+                          newRot = rotation(event);
+                          float r = newRot - d_idk;
+                          float[] values = new float[9];
+                          matrix.getValues(values);
+                          float tx = values[2];
+                          float ty = values[5];
+                          float sx = values[0];
+                          float xc = (imageview.getWidth() / 2) * sx;
+                          float yc = (imageview.getHeight() / 2) * sx;
+                          matrix.postRotate(r, tx + xc, ty + yc);
+                      }
+                  }
+                  break;
           }
-          return false;
 
-      }
-      private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-          @Override
-          public boolean onScale(ScaleGestureDetector detector) {
-              mScaleFactor *= detector.getScaleFactor(); // scale change since previous event
-
-              // Don't let the object get too small or too large.
-              mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
-
-              return true;
-          }
+          imageview.setImageMatrix(matrix);
+          return true;
       }
 
+      /**
+       * Determine the space between the first two fingers
+       */
+      private float spacing(MotionEvent event) {
+          float x = event.getX(0) - event.getX(1);
+          float y = event.getY(0) - event.getY(1);
+          return FloatMath.sqrt(x * x + y * y);
+      }
 
-/*
+      /**
+       * Calculate the mid point of the first two fingers
+       */
+      private void midPoint(PointF point, MotionEvent event) {
+          float x = event.getX(0) + event.getX(1);
+          float y = event.getY(0) + event.getY(1);
+          point.set(x / 2, y / 2);
+      }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
+      /**
+       * Calculate the degree to be rotated by.
+       *
+       * @param event
+       * @return Degrees
+       */
+      private float rotation(MotionEvent event) {
+          double delta_x = (event.getX(0) - event.getX(1));
+          double delta_y = (event.getY(0) - event.getY(1));
+          double radians = Math.atan2(delta_y, delta_x);
+          return (float) Math.toDegrees(radians);
+      }
 
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onDown: " + event.toString());
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent event1, MotionEvent event2,
-                               float velocityX, float velocityY) {
-            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
-            return true;
-        }
-    } */
   }
 }
